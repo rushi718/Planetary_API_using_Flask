3226a6f62d58d5c36a6cfa122517b59b8547e79a
rushi718 <grushikesh718@gmail.com>
2021-02-08 23:34:42 +0530

    Register functionality added

--- app.py
+++ app.py
@@ -96,7 +96,20 @@ def planets():
     result = planets_schema.dump(planets_list)
     return jsonify(result)
 
-
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    test = User.query.filter_by(email=email).first()
+    if test:
+        return jsonify(message='That email already exists.'), 409
+    else:
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
+        user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(message="User created successfully."), 201
 # database models
 class User(db.Model):
     __tablename__ = 'users'
